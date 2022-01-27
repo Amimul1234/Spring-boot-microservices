2275d5eb792cd9de965fea8f9232b03b953020ad
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2022-01-28 02:42:28 +0600

    Added eureka client to customer.

--- customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
@@ -2,12 +2,14 @@ package com.amigoscode.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author Amimul Ehsan
  * @Project amigosservices
  */
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CustomerApplication {
     public static void main( String[] args ) {
