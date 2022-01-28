ba099ae9942b466c6ebcdac3839bca1fe68aa32f
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2022-01-28 20:01:48 +0600

    Added open feign to project.

---
+++ clients/src/main/java/com/amigoscode/clients/fraud/FraudCheckResponse.java
@@ -0,0 +1,10 @@
+package com.amigoscode.clients.fraud;
+
+/**
+ * @Author Amimul Ehsan
+ * @Project amigosservices
+ */
+
+public record FraudCheckResponse(Boolean isFraudster) {
+
+}
--- customer/src/main/java/com/amigoscode/customer/CustomerConfig.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerConfig.java
@@ -1,8 +1,10 @@
 package com.amigoscode.customer;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
 
 /**
  * @Author Amimul Ehsan
@@ -12,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class CustomerConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
