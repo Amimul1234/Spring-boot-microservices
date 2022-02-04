c9b4da10f2eedf5f921974302bf399d94f0f2469
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2022-02-04 22:09:27 +0600

    Added api gate way to project

---
+++ api_gateway/src/main/java/com/amigoscode/apigw/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.amigoscode.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Amimul Ehsan
+ * @Project amigosservices
+ */
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
